fcdbdb7e1380a72a09b2261646d7d9ad3305e98d
5410035 <5410035sryuji331@gmail.com>
2025-01-05 20:22:48 +0900

    250105_テスト完了

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,5 +1,19 @@
+// ハンバーガーメニュー
+var hamburger = $('.hamburger');
+// OPEN/CLOSEボタンをクリックしたら
+$('.header__hamburger').on('click', function () {
+    // .hamburgerの表示・非表示を繰り返す
+    hamburger.toggleClass('hamburger__menu-active');
+});
+// 画面幅のサイズが変わったら
+$(window).on('resize', function () {
+    // ハンバーガーメニューを閉じる
+    hamburger.removeClass('hamburger__menu-active');
+});
+
+// slick
 $(function () {
-    $(".voice__container-customer-slick").slick({
+    $(".gallery__container-headiing-slick").slick({
         arrows: false,
         autoplay: false,
         adaptiveHeight: true,
@@ -7,17 +21,14 @@ $(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         fade: false,
-        initialSlide: 1,
-        centerMode: true,
-        centerPadding: "0rem",
-        responsive: [
-            {
-                breakpoint: 767.9,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
+        initialSlide: 0,
     });
+});
+
+// アコーディオン
+$('.question__container-accordion-box-title').on('click', function (e) {
+    $(this).next().slideToggle();
+    $(this).find(".question__container-accordion-box-title-plus").toggleClass("question__container-accordion-box-title-open", 300);
+    $(this).find(".question__container-accordion-box-title-minus").toggleClass("question__container-accordion-box-title-close", 300);
+    $(this).find(".question__container-accordion-box-content").toggleClass("question__container-accordion-box-title-open", 300);
 });
